b288a5a7bdb1d7994cd8330c36cc7008b4978184
ng-nest-moon <ngnest@163.com>
2021-04-13 22:17:29 +0800

    docs(): add rbac user manage

---
+++ RBAC/5-user-manage/api/src/system/users/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    public readonly usersRepository: Repository<User>,
+  ) {}
+}
