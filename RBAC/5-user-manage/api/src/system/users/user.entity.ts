f15c5e08797e3ba15871060d8c9c38750b10870b
ng-nest-moon <ngnest@163.com>
2021-04-14 22:43:53 +0800

    feat(rbac): user manage add API interfaces

--- RBAC/5-user-manage/api/src/system/users/user.entity.ts
+++ RBAC/5-user-manage/api/src/system/users/user.entity.ts
@@ -2,18 +2,33 @@ import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('system_user')
 export class User {
+  /**
+   * 编码
+   */
   @PrimaryColumn('uuid', { length: 36 })
   id: string;
 
+  /**
+   * 名称
+   */
   @Column({ length: 36 })
   name: string;
 
+  /**
+   * 账号
+   */
   @Column()
   account: string;
 
+  /**
+   * 邮箱
+   */
   @Column()
   email: string;
 
+  /**
+   * 手机号
+   */
   @Column()
   phone: string;
 }
