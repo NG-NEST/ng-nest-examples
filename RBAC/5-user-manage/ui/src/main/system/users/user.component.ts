c512ca7943219b196794251892b6462239bd47d1
ng-nest-moon <ngnest@163.com>
2021-04-19 23:01:44 +0800

    feat(rbac): add ui user manage

--- RBAC/5-user-manage/ui/src/main/system/users/user.component.ts
+++ RBAC/5-user-manage/ui/src/main/system/users/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { XQuery } from '@ng-nest/ui/core';
+import { XTableColumn } from '@ng-nest/ui/table';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-user',
@@ -6,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  constructor() {}
+  columns: XTableColumn[] = [
+    { id: 'index', label: '序号', width: 80, type: 'index' },
+    { id: 'name', label: '姓名', width: 130 },
+    { id: 'account', label: '用户', width: 100 },
+    { id: 'email', label: '邮箱', flex: 1 },
+    { id: 'phone', label: '手机号', flex: 1 }
+  ];
+
+  data = (index: number, size: number, query: XQuery) =>
+    this.userService.getList({ index, size, ...query });
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {}
+
+  pageTo(url: string) {
+    this.router.navigate([url], { relativeTo: this.activatedRoute });
+  }
 }
