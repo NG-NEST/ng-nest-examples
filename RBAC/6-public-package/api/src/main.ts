b537fc69243449a5616101b437564d9cf45c7892
ng-nest-moon <ngnest@163.com>
2021-04-22 19:53:22 +0800

    feat(rbac): add public package

---
+++ RBAC/6-public-package/api/src/main.ts
@@ -0,0 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('rbac-api')
+    .setDescription('The rbac api description')
+    .setVersion('1.0')
+    .addTag('rbac-api')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
---
+++ RBAC/6-public-package/api/src/system/users/user.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('system_user')
+export class User {
+  /**
+   * 编码
+   */
+  @PrimaryColumn('uuid', { length: 36, comment: '编码' })
+  id: string;
+
+  /**
+   * 名称
+   */
+  @Column({ length: 36, comment: '名称' })
+  name: string;
+
+  /**
+   * 账号
+   */
+  @Column()
+  account: string;
+
+  /**
+   * 邮箱
+   */
+  @Column()
+  email: string;
+
+  /**
+   * 手机号
+   */
+  @Column()
+  phone: string;
+}
